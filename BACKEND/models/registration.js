dfbc6f16c1ed004afd706bb98f648a0d4969e4bb
Shivanandh <botkulshiva679@gmail.com>
2025-12-01 11:20:38 +0530

    Update registration.js

--- BACKEND/models/registration.js
+++ BACKEND/models/registration.js
@@ -4,12 +4,7 @@ const registrationSchema = new mongoose.Schema({
     username: { type: String, required: true },
     password: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    contact: { type: String, required: true },
-
-    // ⭐ ADDED FOR OTP
-    otp: { type: String, default: null },
-    otpExpiry: { type: Date, default: null }
-
+    contact: { type: String, required: true }
 }, { timestamps: true });   // ⭐ IMPORTANT
 
 module.exports = mongoose.model("Registration", registrationSchema);
