9a93fe1d671420516fffb0ec9573ed7b6fc68940
Shivanandh <botkulshiva679@gmail..com>
2025-12-06 11:17:25 +0530

    Install google-auth-library for Google Login

--- BACKEND/models/registration.js
+++ BACKEND/models/registration.js
@@ -6,10 +6,6 @@ const registrationSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true },
     contact: { type: String, required: true },
 
-    // ⭐ ADDED FOR OTP
-    otp: { type: String, default: null },
-    otpExpiry: { type: Date, default: null }
-
 }, { timestamps: true });   // ⭐ IMPORTANT
 
 module.exports = mongoose.model("Registration", registrationSchema);
